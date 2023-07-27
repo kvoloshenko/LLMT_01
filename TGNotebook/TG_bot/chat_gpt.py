98ecfd58630d2cc54fb7811bf744e94c370f0e98
kvoloshenko <kvoloshenko@gmail.com>
2023-07-28 00:00:04 +0300

    Refactoring

--- TGNotebook/TG_bot/chat_gpt.py
+++ TGNotebook/TG_bot/chat_gpt.py
@@ -93,23 +93,23 @@ def answer(system, topic, temp = 1):
 
 # def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
 def num_tokens_from_messages(messages, model=ll_model):
+    """Возвращает количество токенов, используемых списком сообщений."""
     try:
-        encoding = tiktoken.encoding_for_model(model)
+        encoding = tiktoken.encoding_for_model(model) # Пытаемся получить кодировку для выбранной модели
     except KeyError:
-        encoding = tiktoken.get_encoding("cl100k_base")
-    # TODO Нужно ли это условие?
-    if model == "gpt-3.5-turbo-0613":
-        num_tokens = 0
-        for message in messages:
-            num_tokens += 4
-            for key, value in message.items():
-                num_tokens += len(encoding.encode(value))
-                if key == "name":
+        encoding = tiktoken.get_encoding("cl100k_base") # если не получается, используем кодировку "cl100k_base"
 
-                    num_tokens += -1
-        num_tokens += 2
-        return num_tokens
-    else:
+    if model == "gpt-3.5-turbo-0301" or "gpt-3.5-turbo-0613" or "gpt-3.5-turbo-16k" or "gpt-3.5-turbo":
+        num_tokens = 0 # начальное значение счетчика токенов
+        for message in messages: # Проходимся по каждому сообщению в списке сообщений
+            num_tokens += 4 # каждое сообщение следует за <im_start>{role/name}\n{content}<im_end>\n, что равно 4 токенам
+            for key, value in message.items(): # итерация по элементам сообщения (роль, имя, контент)
+                num_tokens += len(encoding.encode(value)) # подсчет токенов в каждом элементе
+                if key == "name": # если присутствует имя, роль опускается
+                    num_tokens += -1 # роль всегда требуется и всегда занимает 1 токен, так что мы вычитаем его, если имя присутствует
+        num_tokens += 2 # каждый ответ начинается с <im_start>assistant, что добавляет еще 2 токена
+        return num_tokens # возвращаем общее количество токенов
+    else: # Если выбранная модель не поддерживается, генерируем исключение
         raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.""")
 
 
