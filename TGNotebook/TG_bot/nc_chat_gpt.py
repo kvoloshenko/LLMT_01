7772a8200535ac207f442b7eadade40ef9af0476
kvoloshenko <kvoloshenko@gmail.com>
2023-08-05 14:54:39 +0300

    Refactoring

--- TGNotebook/TG_bot/nc_chat_gpt.py
+++ TGNotebook/TG_bot/nc_chat_gpt.py
@@ -7,7 +7,30 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 import openai
 from dotenv import load_dotenv
+import logging
+import datetime
 
+# XML теги для лога
+LOG_S = '<log>'
+LOG_E = '</log>'
+X_CDATA_S = '<![CDATA['
+X_CDATA_E = ']]>'
+PROMPT_S = '<prompt>' + X_CDATA_S
+PROMPT_E = X_CDATA_E + '</prompt>'
+KNOWLEDGE_DB_S = '<kdb>' + X_CDATA_S
+KNOWLEDGE_DB_E = X_CDATA_E + '</kdb>'
+MESSAGE_CONTENT_S = '<mc>' + X_CDATA_S
+MESSAGE_CONTENT_E = X_CDATA_E + '</mc>'
+
+# Get the current date and time
+current_datetime = datetime.datetime.now()
+
+# Format the date and time as a string
+formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+
+logfilename = "Logs/" + formatted_datetime + "_tgbot_gpt.xml"
+logging.getLogger("faiss").setLevel(logging.ERROR)
+logging.basicConfig(level=logging.INFO, filename=logfilename,filemode="w")
 
 load_dotenv()
 # Загрузка значений из .env
@@ -15,26 +38,24 @@ API_KEY = os.environ.get("API_KEY")
 os.environ["OPENAI_API_KEY"] = API_KEY
 openai.api_key = API_KEY
 
+logging.info(LOG_S)
 LL_MODEL = os.environ.get("LL_MODEL") # модель
-print (f'LL_MODEL = {LL_MODEL}')
+logging.info(f'LL_MODEL = {LL_MODEL}')
 
 CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE")) # Количество токинов в  чанке
-print(f'CHUNK_SIZE={CHUNK_SIZE}')
+logging.info(f'CHUNK_SIZE={CHUNK_SIZE}')
 
 NUMBER_RELEVANT_CHUNKS = int(os.environ.get("NUMBER_RELEVANT_CHUNKS"))   # Количество релевантных чанков
-print(f'NUMBER_RELEVANT_CHUNKS={NUMBER_RELEVANT_CHUNKS}')
-
-VERBOSE = int(os.environ.get("VERBOSE")) # Выводить тех. инфу
-print(f'VERBOSE={VERBOSE}')
+logging.info(f'NUMBER_RELEVANT_CHUNKS={NUMBER_RELEVANT_CHUNKS}')
 
 TEMPERATURE = float(os.environ.get("TEMPERATURE")) # Температура модели
-print(f'TEMPERATURE={TEMPERATURE}')
+logging.info(f'TEMPERATURE={TEMPERATURE}')
 
 SYSTEM_DOC_URL = os.environ.get("SYSTEM_DOC_URL") # промпт
-print (f'SYSTEM_DOC_URL = {SYSTEM_DOC_URL}')
+logging.info(f'SYSTEM_DOC_URL = {SYSTEM_DOC_URL}')
 
 KNOWLEDGE_BASE_URL = os.environ.get("KNOWLEDGE_BASE_URL") # база знаний
-print (f'KNOWLEDGE_BASE_URL = {KNOWLEDGE_BASE_URL}')
+logging.info(f'KNOWLEDGE_BASE_URL = {KNOWLEDGE_BASE_URL}')
 
 
 def load_document_text(url: str) -> str:
@@ -53,11 +74,29 @@ def load_document_text(url: str) -> str:
 
 # Инструкция для GPT, которая будет подаваться в system
 system = load_document_text(SYSTEM_DOC_URL)  # Загрузка файла с Промтом
-if VERBOSE: print(f'\n ===========================================\nsystem={system}')
+logging.info(f'{PROMPT_S}{system}{PROMPT_E}')
 
 # База знаний, которая будет подаваться в LangChain
 database = load_document_text(KNOWLEDGE_BASE_URL)  # Загрузка файла с Базой Знаний
-if VERBOSE: print(f'\n ===========================================\ndatabase={database}\n ===========================================')
+logging.info(f'{KNOWLEDGE_DB_S}{database}{KNOWLEDGE_DB_E}')
+
+source_chunks = []
+splitter = CharacterTextSplitter(separator="\n", chunk_size=1024, chunk_overlap=0)
+
+for chunk in splitter.split_text(database):
+    source_chunks.append(Document(page_content=chunk, metadata={}))
+
+# Инициализирум модель эмбеддингов
+embeddings = OpenAIEmbeddings()
+
+# Создадим индексную базу из разделенных фрагментов текста
+db = FAISS.from_documents(source_chunks, embeddings)
+
+for chunk in source_chunks:  # Поиск слишком больших чанков
+    if len(chunk.page_content) > CHUNK_SIZE:
+        logging.warning(f'*** Слишком большой кусок! ***')
+        logging.warning(f'chunk_len ={len(chunk.page_content)}')
+        logging.warning(f'content ={chunk.page_content}')
 
 # Функция, которая позволяет выводить ответ модели в удобочитаемом виде
 def insert_newlines(text: str, max_len: int = 170) -> str:
@@ -72,62 +111,32 @@ def insert_newlines(text: str, max_len: int = 170) -> str:
     lines.append(current_line)
     return " ".join(lines)
 
-def answer_index(system, topic, search_index, temp=TEMPERATURE, verbose=VERBOSE):
+def answer_index(system, topic, index_db, temp=TEMPERATURE):
 
     # Поиск релевантных отрезков из базы знаний
-    docs = search_index.similarity_search(topic, k = NUMBER_RELEVANT_CHUNKS)
-    # print(f'type(docs)={type(docs)}')
+    docs = index_db.similarity_search(topic, k = NUMBER_RELEVANT_CHUNKS)
 
-    if verbose: print('\n ===========================================: ')
-    message_content = re.sub(r'\n{2}', ' ', '\n '.join([f'\n===================== Отрывок документа №{i+1} =====================\n' + doc.page_content + '\n' for i, doc in enumerate(docs)]))
-    if verbose: print('message_content :\n ======================================== \n', message_content)
+    message_content = re.sub(r'\n{2}', ' ', '\n '.join([f'\n ===================== Отрывок документа №{i+1} =====================\n' + doc.page_content + '\n' for i, doc in enumerate(docs)]))
+    logging.info(f'{MESSAGE_CONTENT_S}{message_content}{MESSAGE_CONTENT_E}')
 
     messages = [
         {"role": "system", "content": system},
-        {"role": "user", "content": f"Документ с информацией для ответа клиенту: {message_content}\n\nВопрос клиента: \n{topic}"}
+        {"role": "user", "content": f"Документ с информацией для ответа клиенту: {message_content}\n\n Вопрос клиента: \n{topic}"}
     ]
-    # print(f'type(messages)={type(messages)}')
-
-    if verbose: print(f'temperature={temp}')
-    if verbose: print('\n ===========================================: ')
 
     completion = openai.ChatCompletion.create(
         model=LL_MODEL,
-        # model = "gpt-3.5-turbo",
         messages=messages,
         temperature=temp
     )
+
     answer = insert_newlines(completion.choices[0].message.content)
+
     return answer  # возвращает ответ
 
 
 def answer_user_question(topic):
-
-    source_chunks = []
-    splitter = CharacterTextSplitter(separator="\n", chunk_size=1024, chunk_overlap=0)
-
-    for chunk in splitter.split_text(database):
-        source_chunks.append(Document(page_content=chunk, metadata={}))
-
-    # Инициализирум модель эмбеддингов
-    embeddings = OpenAIEmbeddings()
-
-    # Создадим индексную базу из разделенных фрагментов текста
-    db = FAISS.from_documents(source_chunks, embeddings)
-
-    # print(f'type(source_chunks)={type(source_chunks)}')
-    # print(f'type(embeddings)={type(embeddings)}')
-    # print(f'type(db)={type(db)}')
-
-    for c in source_chunks: # Поиск слишком больших чанков
-        if len(c.page_content) > CHUNK_SIZE:
-            print(f'chunk_len ={len(c.page_content)}')
-            print(f'content ={c.page_content}')
-
     ans = answer_index(system, topic, db)  # получите ответ модели
-    # print(f'type(ans)={type(ans)}')
-    # print(ans)
-
     return ans
 
 def do_test(topic):
