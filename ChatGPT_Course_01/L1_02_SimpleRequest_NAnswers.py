394a68f893bd7a15897610e2ad8480e830368491
kvoloshenko <kvoloshenko@gmail.com>
2023-08-06 22:54:54 +0300

    Refactoring

---
+++ ChatGPT_Course_01/L1_02_SimpleRequest_NAnswers.py
@@ -0,0 +1,32 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+# Loading values from .env file
+load_dotenv()
+API_KEY = os.environ.get("API_KEY")
+os.environ["OPENAI_API_KEY"] = API_KEY
+openai.api_key = API_KEY
+
+# LLM: gpt-3.5-turbo-0613, gpt-3.5-turbo-0301, gpt-3.5-turbo-16k, gpt-3.5-turbo
+LL_MODEL = "gpt-3.5-turbo-0613"
+print(f'LL_MODEL = {LL_MODEL}')
+
+Number_Answers = 2 # Количество ответов
+
+# Interact with the OpenAI API
+result = openai.ChatCompletion.create(
+  model=LL_MODEL,
+  messages=[
+        {"role": "system", "content": "You are a diligent assistant."},
+        {"role": "user", "content": "who won the f1 championship in 2021?"}
+    ],
+    n=Number_Answers
+)
+
+print(result)  # query result
+# message = result['choices'][0]['message']['content']
+# print(f'message={message}')
+for i, choice in enumerate(result['choices']):
+    message = choice['message']['content']
+    print(f"Answer {i+1}: {message}")
