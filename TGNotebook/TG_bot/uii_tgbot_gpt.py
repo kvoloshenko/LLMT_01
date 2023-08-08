ce013daa350816dcd0d7e3d78b3c1e5c47566069
kvoloshenko <kvoloshenko@gmail.com>
2023-08-08 19:51:26 +0300

    Refactoring

--- TGNotebook/TG_bot/uii_tgbot_gpt.py
+++ TGNotebook/TG_bot/uii_tgbot_gpt.py
@@ -62,9 +62,7 @@ async def text(update, context):
 
     topic = update.message.text
     question_filter_len = len (QUESTION_FILTER)
-    print(f'question_filter_len={question_filter_len}')
     topic_first_n = topic[:question_filter_len]
-    print(f'topic_first_n={topic_first_n}')
 
     chat_type = update.message.chat.type
 
