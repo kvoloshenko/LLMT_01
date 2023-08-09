76dcd02887fd407f5e943fcb088b1bf3cc7bf5f9
kvoloshenko <kvoloshenko@gmail.com>
2023-08-10 01:06:15 +0300

    Refactoring

--- ChatGPT_Course_01/L1_04_Text2chunks.py
+++ ChatGPT_Course_01/L1_04_Text2chunks.py
@@ -7,7 +7,14 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.document_loaders import TextLoader
 
+# Loading values from .env file
+load_dotenv()
+API_KEY = os.environ.get("API_KEY")
+os.environ["OPENAI_API_KEY"] = API_KEY
+openai.api_key = API_KEY
+
 CHUNK_SIZE = 1024                 # Количество токинов в  чанке
+NUMBER_RELEVANT_CHUNKS = 5        # Количество релевантных чанков
 
 loader = TextLoader("Docs/База знаний УИИ.txt", encoding='utf8')
 documents = loader.load()
@@ -32,3 +39,52 @@ docs[0].metadata["teg"] = "Описание УИИ"
 
 # выводим на печать обновленные метаданные чанка
 print(f'metadata псле = {docs[0].metadata}')
+
+# Инициализирум модель эмбеддингов
+embeddings = OpenAIEmbeddings()
+
+# Создадим индексную базу из разделенных фрагментов текста
+# https://python.langchain.com/docs/integrations/vectorstores/faiss
+db = FAISS.from_documents(docs, embeddings)
+
+# Поиск текста по схожести
+# similarity_search
+print('similarity_search')
+query = "нужно ли знать математику?"
+docs = db.similarity_search(query, k=NUMBER_RELEVANT_CHUNKS)
+
+i=1
+for item in docs:
+    print(f'{i} item.page_content={item.page_content}\n\n')
+    i += 1
+
+# similarity_search_with_score
+print('similarity_search_with_score')
+docs_and_scores = db.similarity_search_with_score(query, k=NUMBER_RELEVANT_CHUNKS)
+
+# print (type(docs_and_scores))
+# print (docs_and_scores)
+i=1
+for doc in docs_and_scores:
+    print(f'{i}')
+    for item in doc:
+        print (item)
+
+    i += 1
+    print(f'\n')
+
+# Нужно проверять условие изменения файла Базы знаний.
+# db.save_local("/content/drive/My Drive/faiss_index")
+db.save_local("DB/faiss_index")
+print('Db saved')
+
+#Load file.
+new_db = db.load_local('DB/faiss_index', embeddings)
+print('Db loaded')
+
+docs = new_db.similarity_search(query, k=NUMBER_RELEVANT_CHUNKS)
+
+i=1
+for item in docs:
+    print(f'{i} item.page_content={item.page_content}\n\n')
+    i += 1
