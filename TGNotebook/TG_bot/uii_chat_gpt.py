26f8c70619d33a3633ee2b8a9c10f948e6a9eb4a
kvoloshenko <kvoloshenko@gmail.com>
2023-08-12 02:03:57 +0300

    Refactoring

--- TGNotebook/TG_bot/uii_chat_gpt.py
+++ TGNotebook/TG_bot/uii_chat_gpt.py
@@ -7,55 +7,30 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 import openai
 from dotenv import load_dotenv
-import logging
-import datetime
 
-# XML теги для лога
-LOG_S = '<log>'
-LOG_E = '</log>'
-X_CDATA_S = '<![CDATA['
-X_CDATA_E = ']]>'
-PROMPT_S = '<prompt>' + X_CDATA_S
-PROMPT_E = X_CDATA_E + '</prompt>'
-KNOWLEDGE_DB_S = '<kdb>' + X_CDATA_S
-KNOWLEDGE_DB_E = X_CDATA_E + '</kdb>'
-MESSAGE_CONTENT_S = '<mc>' + X_CDATA_S
-MESSAGE_CONTENT_E = X_CDATA_E + '</mc>'
-
-# Get the current date and time
-current_datetime = datetime.datetime.now()
-
-# Format the date and time as a string
-formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
-
-logfilename = "Logs/" + formatted_datetime + "_tgbot_gpt.xml"
-logging.getLogger("faiss").setLevel(logging.ERROR)
-logging.basicConfig(level=logging.INFO, filename=logfilename,filemode="w")
-
+# Loading values from .env file
 load_dotenv()
-# Загрузка значений из .env
 API_KEY = os.environ.get("API_KEY")
 os.environ["OPENAI_API_KEY"] = API_KEY
 openai.api_key = API_KEY
 
-# модель: gpt-3.5-turbo-0613, gpt-3.5-turbo-16k, gpt-3.5-turbo
+# LLM: gpt-3.5-turbo-0613, gpt-3.5-turbo-0301, gpt-3.5-turbo-16k, gpt-3.5-turbo
 LL_MODEL = "gpt-3.5-turbo-0613"
 print(f'LL_MODEL = {LL_MODEL}')
 
-CHUNK_SIZE = 1024  # Количество токинов в  чанке
+CHUNK_SIZE = 1024  # Number of tokens in a chunk
 print(f'CHUNK_SIZE={CHUNK_SIZE}')
 
-NUMBER_RELEVANT_CHUNKS = 5  # Количество релевантных чанков
+NUMBER_RELEVANT_CHUNKS = 5  # Number of relevant chunks
 print(f'NUMBER_RELEVANT_CHUNKS={NUMBER_RELEVANT_CHUNKS}')
 
-TEMPERATURE = 1 # Температура модели
+TEMPERATURE = 0.5 # Model temperature parameter controlling the randomness of the response
 print(f'TEMPERATURE={TEMPERATURE}')
 
-
-SYSTEM_DOC_URL = 'https://docs.google.com/document/d/1WHHMJS1zZDWZHjawy3zQjhTwm4JsHgWI'    # промпт
+SYSTEM_DOC_URL = 'https://docs.google.com/document/d/1eW_hbYfvLM38n4X5nc6u9oEaV4wuPeio' # Prompt
 print(f'SYSTEM_DOC_URL = {SYSTEM_DOC_URL}')
 
-KNOWLEDGE_BASE_URL = 'https://docs.google.com/document/d/1lWUDP5LVlxXb5jU2XxjbgrqYhyvqtr_i/' # база знаний
+KNOWLEDGE_BASE_URL = 'https://docs.google.com/document/d/1LgPsHoy3YA2wfnKRjW05a1_NztZVcdH7' # Knowledge base
 print(f'KNOWLEDGE_BASE_URL = {KNOWLEDGE_BASE_URL}')
 
 def load_document_text(url: str) -> str:
@@ -72,59 +47,37 @@ def load_document_text(url: str) -> str:
 
     return text
 
-# Инструкция для GPT, которая будет подаваться в system
+# Instruction for GPT to be sent to system
 system = load_document_text(SYSTEM_DOC_URL)  # Загрузка файла с Промтом
-logging.info(f'{PROMPT_S}{system}{PROMPT_E}')
+# print(f'PROMPT={system}')
 
-# База знаний, которая будет подаваться в LangChain
+# Knowledge base to be sent into LangChain
 database = load_document_text(KNOWLEDGE_BASE_URL)  # Загрузка файла с Базой Знаний
-logging.info(f'{KNOWLEDGE_DB_S}{database}{KNOWLEDGE_DB_E}')
+# print(f'KNOWLEDGE={database}')
 
 source_chunks = []
-splitter = CharacterTextSplitter(separator="\n", chunk_size=1024, chunk_overlap=0)
+splitter = CharacterTextSplitter(separator="\n", chunk_size=CHUNK_SIZE, chunk_overlap=0)
 
 for chunk in splitter.split_text(database):
     source_chunks.append(Document(page_content=chunk, metadata={}))
 
-# Инициализирум модель эмбеддингов
+# Initializing the embedding model
 embeddings = OpenAIEmbeddings()
 
-try:
-    db = FAISS.from_documents(source_chunks, embeddings) # Создадим индексную базу из разделенных фрагментов текста
-except Exception as e: # обработка ошибок openai.error.RateLimitError
-    print(f'!!! External error, see log !!!')
-    logging.error(f'!!! External error: {str(e)}')
-
-for chunk in source_chunks:  # Поиск слишком больших чанков
-    if len(chunk.page_content) > CHUNK_SIZE:
-        logging.warning(f'*** Слишком большой кусок! ***')
-        logging.warning(f'chunk_len ={len(chunk.page_content)}')
-        logging.warning(f'content ={chunk.page_content}')
-
-# Функция, которая позволяет выводить ответ модели в удобочитаемом виде
-def insert_newlines(text: str, max_len: int = 170) -> str:
-    words = text.split()
-    lines = []
-    current_line = ""
-    for word in words:
-        if len(current_line + " " + word) > max_len:
-            lines.append(current_line)
-            current_line = ""
-        current_line += " " + word
-    lines.append(current_line)
-    return " ".join(lines)
+# Create an index db from separated text fragments
+db = FAISS.from_documents(source_chunks, embeddings)
 
 def answer_index(system, topic, index_db, temp=TEMPERATURE):
 
-    # Поиск релевантных отрезков из базы знаний
+    # Search for relevant segments from the knowledge base
     docs = index_db.similarity_search(topic, k = NUMBER_RELEVANT_CHUNKS)
 
-    message_content = re.sub(r'\n{2}', ' ', '\n '.join([f'\n ===================== Отрывок документа №{i+1} =====================\n' + doc.page_content + '\n' for i, doc in enumerate(docs)]))
-    logging.info(f'{MESSAGE_CONTENT_S}{message_content}{MESSAGE_CONTENT_E}')
+    message_content = re.sub(r'\n{2}', ' ', '\n '.join([f'\n ===================== Document segment №{i+1} =====================\n' + doc.page_content + '\n' for i, doc in enumerate(docs)]))
+    print(f'message_content={message_content}')
 
     messages = [
         {"role": "system", "content": system},
-        {"role": "user", "content": f"Документ с информацией для ответа клиенту: {message_content}\n\n Вопрос клиента: \n{topic}"}
+        {"role": "user", "content": f"The document with information to respond to the Customer: {message_content}\n\n Customer Question: \n{topic}"}
     ]
 
     completion = openai.ChatCompletion.create(
@@ -133,9 +86,9 @@ def answer_index(system, topic, index_db, temp=TEMPERATURE):
         temperature=temp
     )
 
-    answer = insert_newlines(completion.choices[0].message.content)
+    answer = completion.choices[0].message.content
 
-    return answer  # возвращает ответ
+    return answer
 
 
 def answer_user_question(topic):
@@ -147,9 +100,11 @@ def do_test(topic):
     return ans
 
 if __name__ == '__main__':
-    topic = 'Привет! Ты кто?'
+    print('---------------------------')
+    topic = 'Hello! Who are you?'
     print(f'topic={topic}')
     response = do_test(topic)
+    print('---------------------------')
     print(f'response={response}')
 
 
---
+++ TGNotebook/TG_bot/uii_tgbot_gpt_00.py
@@ -0,0 +1,102 @@
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from dotenv import load_dotenv
+import time
+import os
+import uii_chat_gpt_00 as chat_gpt
+import logging
+
+# XML теги для лога
+LOG_S = '<log>'
+LOG_E = '</log>'
+X_CDATA_S = '<![CDATA['
+X_CDATA_E = ']]>'
+MESSAGE_TEXT_S = '<mt>' + X_CDATA_S
+MESSAGE_TEXT_E = X_CDATA_E + '</mt>'
+MESSAGE_DATE_S = '<md>'
+MESSAGE_DATE_E = '</md>'
+MESSAGE_ID_S = '<mi>'
+MESSAGE_ID_E = '</mi>'
+USER_NAME_S = '<un>'
+USER_NAME_E = '</un>'
+USER_ID_S = '<ui>'
+USER_ID_E = '</ui>'
+REPLY_TEXT_S = '<rt>' + X_CDATA_S
+REPLY_TEXT_E = X_CDATA_E + '</rt>'
+
+logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("telegram").setLevel(logging.ERROR)
+
+# возьмем переменные окружения из .env
+load_dotenv()
+
+# загружаем значеняи из файла .env
+TOKEN = os.environ.get("TOKEN")
+
+TEXT_BEGINNING = '*** Bot is talking to you: ***'
+print(f'TEXT_BEGINNING = {TEXT_BEGINNING}')
+TEXT_END = '*** Check the information with the manager! ***'
+print (f'TEXT_END = {TEXT_END}')
+
+QUESTION_FILTER = '@userTest01bot'
+
+# функция команды /start
+async def start(update, context):
+  await update.message.reply_text('Привет! Это update_context бот.')
+
+# функция для текстовых сообщений
+async def text(update, context):
+    # использование update
+    logging.info(f'{MESSAGE_DATE_S}{update.message.date}{MESSAGE_DATE_E}')
+    logging.info(f'{MESSAGE_ID_S}{update.message.message_id}{MESSAGE_ID_E}')
+    logging.info(f'{USER_NAME_S}{update.message.from_user.first_name}{USER_NAME_E}')
+    logging.info(f'{USER_ID_S}{update.message.from_user.id}{USER_ID_E}')
+    logging.info(f'{MESSAGE_TEXT_S}{update.message.text}{MESSAGE_TEXT_E}')
+    print('-------------------')
+    print(f'update: {update}')
+    print(f'date: {update.message.date}')
+    print(f'id message: {update.message.message_id}')
+    print(f'name: {update.message.from_user.first_name}')
+    print(f'user.id: {update.message.from_user.id}')
+    print(f'text: {update.message.text}')
+
+
+    topic = update.message.text
+    question_filter_len = len (QUESTION_FILTER)
+    topic_first_n = topic[:question_filter_len]
+
+    chat_type = update.message.chat.type
+
+    if (QUESTION_FILTER == topic_first_n) or (chat_type == 'private'):
+        reply_text = chat_gpt.answer_user_question(topic)
+        response = TEXT_BEGINNING + '\n'
+        response = response + reply_text + '\n' + TEXT_END
+
+        my_message = await update.message.reply_text(f'{response}')
+        logging.info(f'{REPLY_TEXT_S}{reply_text}{REPLY_TEXT_E}')
+        print(f'reply_text: {reply_text}')
+        print('-------------------')
+
+
+def main():
+
+    # точка входа в приложение
+    application = Application.builder().token(TOKEN).build()
+    print('Бот запущен..!')
+    # logging.info(LOG_S)
+
+    # добавляем обработчик команды /start
+    application.add_handler(CommandHandler("start", start))
+
+    # добавляем обработчик текстовых сообщений
+    application.add_handler(MessageHandler(filters.TEXT, text))
+
+    # запуск приложения (для остановки нужно нажать Ctrl-C)
+    application.run_polling()
+
+    print('Бот остановлен..!')
+    logging.info(LOG_E)
+
+
+
+if __name__ == "__main__":
+    main()
