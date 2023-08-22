cb780d083121100748d8e3abece8cacbf87a43bd
kvoloshenko <kvoloshenko@gmail.com>
2023-08-22 18:05:15 +0300

    Refactoring

--- TGNotebook/TG_bot/load_document_text_test.py
+++ TGNotebook/TG_bot/load_document_text_test.py
@@ -6,13 +6,19 @@ def load_document_text(url: str) -> str:
     if match_ is None:
         raise ValueError('Invalid Google Docs URL')
     doc_id = match_.group(1)
+    print (f'doc_id={doc_id}')
     text = ''
 
     try:
         # Check if you have permission to view the document
-        permission_check_url = f'https://docs.google.com/drive/v3/files/{doc_id}/permissions'
+        # permission_check_url = f'https://docs.google.com/drive/v3/files/{doc_id}/permissions'
+        permission_check_url = f'https://docs.google.com/document/d/{doc_id}'
+        print(f'permission_check_url={permission_check_url}')
         response = requests.get(permission_check_url)
+        status_code = response.status_code
+        print(f'status_code={status_code}')
         response.raise_for_status()
+
 
         # Download the document as plain text
         response = requests.get(f'https://docs.google.com/document/d/{doc_id}/export?format=txt')
