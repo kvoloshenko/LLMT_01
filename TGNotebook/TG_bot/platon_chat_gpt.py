6ac59a632bad8566e6a0594841b29196cf848c99
kvoloshenko <kvoloshenko@gmail.com>
2023-08-31 22:09:23 +0300

    Refactoring

--- TGNotebook/TG_bot/platon_chat_gpt.py
+++ TGNotebook/TG_bot/platon_chat_gpt.py
@@ -171,12 +171,12 @@ def answer_index(system, topic, index_db, temp=TEMPERATURE):
     # Поиск релевантных отрезков из базы знаний
     docs = index_db.similarity_search(topic, k = NUMBER_RELEVANT_CHUNKS)
 
-    message_content = re.sub(r'\n{2}', ' ', '\n '.join([f'\n ===================== Отрывок документа №{i+1} =====================\n' + doc.page_content + '\n' for i, doc in enumerate(docs)]))
+    message_content = re.sub(r'\n{2}', ' ', '\n '.join([f'\n#### Document excerpt №{i+1}####\n' + doc.page_content + '\n' for i, doc in enumerate(docs)]))
     logging.info(f'{MESSAGE_CONTENT_S}{message_content}{MESSAGE_CONTENT_E}')
 
     messages = [
         {"role": "system", "content": system},
-        {"role": "user", "content": f"Документ с информацией для ответа клиенту: {message_content}\n\n Вопрос клиента: \n{topic}"}
+        {"role": "user", "content": f"Here is the document with information to respond to the client: {message_content}\n\n Here is the client's question: \n{topic}"}
     ]
 
     num_tokens = num_tokens_from_messages(messages, LL_MODEL)
