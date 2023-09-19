121c524afa677b8c1f1b7c6f9422703c02098755
kvoloshenko <kvoloshenko@gmail.com>
2023-09-19 22:30:09 +0300

    Refactoring

--- TGNotebook/TG_bot/langchain_test_02.py
+++ TGNotebook/TG_bot/langchain_test_02.py
@@ -1,27 +1,47 @@
-# https://www.sbert.net/examples/applications/retrieve_rerank/README.html#pre-trained-bi-encoders-retrieval
-import os
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.docstore.document import Document
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-import openai
-from dotenv import load_dotenv
-
+# In this example, we first initialize a Sentence-BERT model
+# and use it to generate embeddings for a list of sentences.
+# Then, we normalize the embeddings and add them to a FAISS index.
+# After adding the embeddings to the index, we can use it to perform similarity search.
+# We encode the query phrase, normalize its embedding and then search the FAISS index
+# to find the most similar sentences in the corpus.
+# The results are sorted by their similarity to the query.
+# Importing necessary libraries
 from sentence_transformers import SentenceTransformer
-
-load_dotenv()
-API_KEY = os.environ.get("API_KEY")
-os.environ["OPENAI_API_KEY"] = API_KEY
-openai.api_key = API_KEY
-database = ''' Это заглушка на содержание Базы Знаний'''
-source_chunks = []
-splitter = CharacterTextSplitter(separator="\n", chunk_size=512, chunk_overlap=0)
-for chunk in splitter.split_text(database):
-    source_chunks.append(Document(page_content=chunk, metadata={}))
-embeddings = OpenAIEmbeddings()
-
-# https://www.sbert.net/docs/pretrained-models/msmarco-v3.html
-model = SentenceTransformer('msmarco-MiniLM-L-6-v3')
+import numpy as np
+import faiss
 
 
-db = FAISS.from_documents(source_chunks, embeddings)
+# Initializing a Sentence-BERT model
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# A list of sentences
+sentences = ['This is an example sentence', 'Another sentence', 'Yet another sentence', 'This is yet another example']
+
+# Getting embeddings for the sentences
+embeddings = model.encode(sentences)
+
+# Normalizing the vectors (optional but usually improves the results)
+embeddings = embeddings / np.linalg.norm(embeddings, axis=1)[:, np.newaxis]
+
+# Constructing a FAISS index
+index = faiss.IndexFlatL2(embeddings.shape[1])
+
+# Adding the vectors to the FAISS index
+index.add(embeddings)
+
+# Now we can perform similarity search using the index
+# For example, let's find the sentences in the corpus that are most similar to "example sentence"
+query = "example sentence"
+query_embedding = model.encode([query])
+query_embedding = query_embedding / np.linalg.norm(query_embedding)
+
+# Searching the index
+D, I = index.search(query_embedding, k=2)
+
+# Printing out the most similar sentences to the query
+print("Sentences most similar to:", query)
+for i in range(I.shape[1]):
+    print(sentences[I[0, i]], "with a score of", D[0, i])
+
+
+
