87e5943509db0502d3c9f8fe07f452917cc5d9c0
kvoloshenko <kvoloshenko@gmail.com>
2023-10-13 17:02:02 +0300

    Refactoring

--- TGNotebook/run_TgBot.bat
+++
@@ -1,3 +0,0 @@
-C:\_Proj\LLMT_01\venv\Scripts\activate.bat
-C:\_Proj\LLMT_01\venv\Scripts\python.exe C:\_Proj\LLMT_01\TGNotebook\TG_bot\openlesson2_tgbot_gpt_02.py
-C:\_Proj\LLMT_01\venv\Scripts\deactivate.bat
