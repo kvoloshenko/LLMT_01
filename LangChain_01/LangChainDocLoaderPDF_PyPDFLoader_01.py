e7b73567c229733f1fa44f9f67f72b243100d252
kvoloshenko <kvoloshenko@gmail.com>
2023-10-25 09:43:31 +0300

    Refactoring

--- LangChain_01/LangChainDocLoaderPDF_PyPDFLoader_01.py
+++ LangChain_01/LangChainDocLoaderPDF_PyPDFLoader_01.py
@@ -1,3 +1,5 @@
+https://python.langchain.com/docs/integrations/document_loaders
+
 import os
 import openai
 from dotenv import load_dotenv
@@ -10,7 +12,8 @@ os.environ["OPENAI_API_KEY"] = API_KEY
 openai.api_key = API_KEY
 
 # loader = PyPDFLoader("VectorDBAnalysis.pdf")
-loader = PyPDFLoader("Jollibee-Dine-In-Menu.pdf")
+# loader = PyPDFLoader("Jollibee-Dine-In-Menu.pdf")
+loader = PyPDFLoader("OCS Audit replication tool-v60-20231024_071654.pdf")
 pages = loader.load_and_split()
 
 print(f'type={type(pages)}, pages={pages}')
